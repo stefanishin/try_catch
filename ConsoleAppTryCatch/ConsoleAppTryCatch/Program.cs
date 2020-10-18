225ba85d72fe8a8778aebe22b23db591b18cc528
Stefanishin Mikita <stefanishin92@gmail.com>
2020-10-18 11:24:05 +0300

    try_catch

---
+++ ConsoleAppTryCatch/ConsoleAppTryCatch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTryCatch
+{
+    public class dev
+    {
+        int x = 5;
+        int y = x / 0;
+
+        Console.WriteLine("{y}");
+    }
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            dev d = new dev();
+            d.GetType();
+        }
+    }
+}
